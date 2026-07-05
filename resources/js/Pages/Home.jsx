fd2623d950ce293f9fedf2e3c681bfc0ed948e83
unknown <rizkypratamalvbis@gmail.com>
2026-07-06 01:32:29 +0700

    Initial commit: project structure

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home({ message }) {
+    return (
+        <div>
+            <h1>Hello Inertia + React!</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import { bunny } from 'laravel-vite-plugin/fonts';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import inertia from '@inertiajs/vite'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
+            refresh: true,
+            fonts: [
+                bunny('Instrument Sans', {
+                    weights: [400, 500, 600],
+                }),
+            ],
+        }),
+        tailwindcss(),
+        react(),
+        inertia(),
+    ],
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    },
+});
